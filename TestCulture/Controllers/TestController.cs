d5464a261eb4abad95906cf1465479eb958bf38c
Ali Servet Donmez <asd@pittle.org>
2014-10-09 15:46:14 +0200

    Initial commit

---
+++ TestCulture/Controllers/TestController.cs
@@ -0,0 +1,41 @@
+﻿namespace TestCulture.Controllers
+{
+    using System.Globalization;
+    using System.Threading;
+    using System.Web.Mvc;
+    using TestCulture.Models;
+
+    public class TestController : Controller
+    {
+        //
+        // GET: /Test/
+
+        public ActionResult Index(string culture)
+        {
+            ////var model = (TestClass)this.TempData["Model"] ?? new TestClass
+            ////{
+            ////    Amount = 7.50m,
+            ////    CreatedAt = DateTime.Now,
+            ////};
+            var model = (TestClass)this.TempData["Model"] ?? new TestClass();
+
+            if (!string.IsNullOrWhiteSpace(culture))
+            {
+                model.Culture = culture;
+
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(culture);
+                Thread.CurrentThread.CurrentCulture = new CultureInfo(culture);
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult TestPost(TestClass model)
+        {
+            this.TempData["Model"] = model;
+
+            return this.RedirectToAction("Index", new { culture = model.Culture });
+        }
+    }
+}
---
+++ TestCulture/Models/TestClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TestCulture.Models
+{
+    public class TestClass
+    {
+        public DateTime CreatedAt { get; set; }
+
+        [Range(10.5D, 20.3D)]
+        public decimal Amount { get; set; }
+
+        public string Culture { get; set; }
+    }
+}
